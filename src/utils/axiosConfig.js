a12e0359a2b4cdf841fe535486e2acc87f2c69f8
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-12-12 19:07:17 +0530

    base url changed

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://sharence-server.onrender.com/api/v1/',
+  baseURL: 'http://localhost:8080/api/v1/',
   timeout: 500000, 
   headers: {
     'Content-Type': 'application/json',
