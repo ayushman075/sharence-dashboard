0deae0b4b13a7bd72dbb2827821835012d43f9f8
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-12-12 19:25:06 +0530

    auth token added

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080/api/v1/',
@@ -9,6 +10,27 @@ const axiosInstance = axios.create({
   withCredentials: true, 
 });
 
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+     
+      const token = Cookies.get('authToken'); 
+  
+      if (token) {
+        
+        config.headers.Authorization = `Bearer ${token}`;
+      } else {
+        console.log("No token found in cookies.");
+        Cookies.remove('authToken'); 
+         window.location.href = '/login';
+      }
+  
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
 
 export default axiosInstance;
